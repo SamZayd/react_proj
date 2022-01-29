b756fec88c3489efb87f00c5c22d451a389ed3b6
SamZayd <sadafk2304@gmail.com>
2022-01-30 00:13:41 +0530

    done

---
+++ Practice Files/State/Datas using map/src/index.js
@@ -0,0 +1,4 @@
+import ReactDOM from 'react-dom'
+import Employees from "./Employees"
+
+ReactDOM.render(<Employees/>,document.getElementById('two'))
---
+++ Practice Files/State/event binding/e.g 1 hike/src/index.js
@@ -0,0 +1,4 @@
+import ReactDOM from 'react-dom'
+import Hike from "./Hike"
+
+ReactDOM.render(<Hike/>,document.getElementById('two'))
