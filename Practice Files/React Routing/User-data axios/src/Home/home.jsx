ec98d8d170569d9dd5fa99a47aa139229314c779
SamZayd <sadafk2304@gmail.com>
2022-02-01 01:44:51 +0530

    axios

---
+++ Practice Files/React Routing/User-data axios/src/Home/home.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class home extends Component {
+  render() {console.log("home working");
+    return <>
+        <div className='container '>
+            <h1>React Routing</h1>
+        </div>    
+        </>;
+  }
+}
+
+export default home;
