0f160b8aa02e82a6ac17095d42f1cf2ea3e58b24
SamZayd <sadafk2304@gmail.com>
2022-02-06 02:53:43 +0530

    updated

---
+++ src/User/Profile.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+
+class Profile extends Component {
+  render() {
+    return <>
+            <div className="container">
+                <div className="card" style={{height:'500px', width:'150%'}}>
+                    <div className="card-header bg-dark text-light">
+                        <h5>Details</h5>
+                    </div>
+                        <div className="card-body">
+                            
+                        </div>
+                    </div>
+                </div>
+        </>
+  }
+}
+
+export default Profile
--- src/User/User.jsx
+++ src/User/User.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Axios from 'axios';
+import Profile from './Profile';
 
 class User extends Component {
     constructor(props) {
@@ -9,7 +10,7 @@ class User extends Component {
         }
     }
     componentDidMount() {
-        Axios.get('https://jsonplaceholder.typicode.com/users')
+        Axios.get('https://gist.githubusercontent.com/narasimhareddyprostack/7e344f346f47bc53a889d78b5258d0c9/raw/56d531cb936d9c79e2417e5d0e5d8c9c876800f2/contactlist')
             .then((response) => {
                 this.setState({ userd: response.data })
             }).catch()
@@ -18,38 +19,41 @@ class User extends Component {
         return (
             <>
                 <div className="conatiner bg-dark mt-2 text-light">
-                    <h1 className='ml-5'>Application Users Data</h1>
+                    <h1 className='mr-5'>Application Users Data</h1>
                 </div>
-                <div className="container">
+                <div className="container ml-1">
                     <div className="row">
                         <div className="col">
                             <table className="table table-hover">
-                                <thead className="bg-dark text-white">
+                                <thead className="bg-dark text-white" style={{display:'block'}}>
                                     <tr>
-                                        <th>Id</th>
-                                        <th>Name</th>
-                                        <th>Email</th>
-                                        <th>Username</th>
-                                        <th>City</th>
-                                        <th>Company</th>
+                                        <th style={{ position: 'sticky',left: '5px'}}>Id</th>
+                                        <th style={{ position: 'sticky',left: '80px'}}>Name</th>
+                                        <th style={{ position: 'sticky',left: '235px'}}>Email</th>
+                                        <th style={{ position: 'sticky',left: '485px'}}>Gender</th>
+                                        <th style={{ position: 'sticky',left: '565px'}}>City</th>
+                                        <th style={{ position: 'sticky',left: '755px'}}>Age</th>
                                     </tr>
                                 </thead>
-                                <tbody>
+                                <tbody style={{display:'block', height: '470px', width: '850px' ,overflowY:'scroll' ,scrollbarWidth:'thin'}}>
                                     {
                                         this.state.userd.map((usern) => {
-                                            return <tr>
-                                                <td>{usern.id}</td>
-                                                <td>{usern.name}</td>
+                                            return <tr key={usern.login.uuid}>
+                                                <td>{usern.login.uuid.substring(32,36)}</td>
+                                                <td>{usern.name.first} {usern.name.last}</td>
                                                 <td>{usern.email}</td>
-                                                <td>{usern.username}</td>
-                                                <td>{usern.address.city}</td>
-                                                <td>{usern.company.name}</td>
+                                                <td>{usern.gender}</td>
+                                                <td>{usern.location.city}</td>
+                                                <td>{usern.dob.age}</td>
                                             </tr>
                                         })
                                     }
 
                                 </tbody>
                             </table>
+                        </div>
+                        <div className="col">
+                            <Profile/>
                         </div>
                     </div>
                 </div>
